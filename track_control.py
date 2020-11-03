d70d9f62a8135f60ee19af43359a4223ddb2ab8a
ericql <eqle22@gmail.com>
2020-11-03 15:46:19 -0800

    Structural comments

--- track_control.py
+++ track_control.py
@@ -1,5 +1,10 @@
 from stepper_control import StepperControl
 
+"""
+    Implementation of bottom track movement. At the moment only focusing on movement for a fixed number
+    of steps before any further implementation.
+"""
+
 class TrackControl:
 
     stepper_motor_track = None
@@ -8,10 +13,5 @@ class TrackControl:
         # init stepper motor
         self.stepper_motor_track = StepperControl(pin_step, pin_direction, pin_ms1, pin_ms2)
 
-    def move_left(self, dir, num_steps):
+    def move_left_right(self, dir, num_steps):
         self.stepper_motor_track.spin_fixed_step(dir, num_steps)
-
-    def move_right(self, dir, num_steps):
-        self.stepper_motor_track.spin_fixed_step(dir, num_steps)
-
-
