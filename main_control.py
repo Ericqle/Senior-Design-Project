2f3d3ebb74a47c61aa0b855129bfa7722fabb53f
ericql <eqle22@gmail.com>
2020-11-03 15:44:39 -0800

    DrawControl class created

---
+++ main_control.py
@@ -0,0 +1,13 @@
+# TODO implementation
+
+"""
+    Primary source for drawing control. Implements the usage of TrackControl and RailControl
+    functions to draw an Image.
+
+"""
+
+
+class DrawControl:
+    track_control = None
+    rail_control = None
+
