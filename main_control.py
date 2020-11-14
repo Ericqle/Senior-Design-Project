f3c098e3687a12704bd846984df3afd1ab64d5fc
ericql <eqle22@gmail.com>
2020-11-14 15:38:17 -0800

    Remove layer of abstraction

--- main_control.py
+++ main_control.py
@@ -1,4 +1,4 @@
-from track_control import TrackControl
+from stepper_control import StepperControl
 
 """
     Primary source for drawing control. Implements the usage of TrackControl and RailControl
@@ -21,10 +21,8 @@ class DrawControl:
     rail = None
 
     def __init__(self):
-        self.track = TrackControl(TRACK_PIN_STEP, TRACK_PIN_DIRECTION, TRACK_PIN_MS1, TRACK_PIN_MS2)
-        self.rail = TrackControl(RAIL_PIN_STEP, RAIL_PIN_DIRECTION, RAIL_PIN_MS1, RAIL_PIN_MS2)
-
-
+        self.track = StepperControl(TRACK_PIN_STEP, TRACK_PIN_DIRECTION, TRACK_PIN_MS1, TRACK_PIN_MS2)
+        self.rail = StepperControl(RAIL_PIN_STEP, RAIL_PIN_DIRECTION, RAIL_PIN_MS1, RAIL_PIN_MS2)
 
 if __name__ == '__main__':
     zotter = DrawControl()
@@ -34,8 +32,8 @@ if __name__ == '__main__':
     dir = int(dir_in)
     num = int(num_in)
     if dir_in:
-        zotter.track.move_fixed(dir, num)
+        zotter.track.spin_fixed_step(dir, num)
         print(dir, "", num)
     else:
-        zotter.track.move_fixed(dir, num)
+        zotter.track.spin_fixed_step(dir, num)
         print(dir, "", num)
