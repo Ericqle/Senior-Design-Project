3b65ea978d00ff9a408e4a01ddb2544c84f4e747
ericql <eqle22@gmail.com>
2020-11-15 21:52:36 -0800

    Initial servo test finished

--- servo_control.py
+++ servo_control.py
@@ -5,10 +5,16 @@ class ServoControl:
     pin_signal = 0;
 
     def __init__(self, pin_signal):
+        # servo pin init
         self.pin_signal = pin_signal
         GPIO.setup(self.pin_signal,GPIO.OUT)
         self.servo = GPIO.PWM(self.pin_signal, 50)
 
+    # Turn a fixed angle:
+    """
+        80: servo tip to pen holder
+        120: pen holder at max height
+    """
     def turn_angle(self, angle):
         self.servo.start(0)
         self.servo.ChangeDutyCycle(2 + (angle / 18))
