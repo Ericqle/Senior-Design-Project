dca1b3961c2a5a6d695131c4d1ad86bc867c5354
ericql <eqle22@gmail.com>
2020-11-24 16:41:30 -0800

    Retesting frequency

---
+++ diag_test.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO, time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setwarnings(False)
+GPIO.output(16, True)
+
+p = GPIO.PWM(16, 5000)
+
+def SpinMotor(direction, num_steps, frequency):
+    p.ChangeFrequency(frequency)
+    GPIO.output(18, direction)
+    while num_steps > 0:
+        p.start(1)
+        time.sleep(0.01)
+        num_steps -= 1
+    p.stop()
+    return True
+
+if __name__ == '__main__':
+
+    dir_in = input('0 cw 1 ccw: ')
+
+    while(dir_in):
+        dir = int(dir_in)
+
+        if (dir == 1) or (dir == 0):
+            num_in = input('num steps: ')
+            num = int(num_in)
+            freq_in = input('freq: ')
+            freq = int(freq_in)
+            SpinMotor(dir, num, freq)
+
+        dir_in = input('0 cw 1 ccw: ')
+
+    GPIO.cleanup()
