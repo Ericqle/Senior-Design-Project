464d0e8e495dc81ab6dc931965815c521280b12d
ericql <eqle22@gmail.com>
2021-01-18 16:15:28 -0800

    move opencv implementation in parser

--- parser.py
+++ parser.py
@@ -1,7 +1,14 @@
 from draw_control import DrawControl
+import cv2 as cv
+
 
 class Parser:
     zotter_plotter = None
+    HEIGHT_PAPER = 150
+    WIDTH_PAPER = 250
+    PRECISION = 50
+
+    instructions = list()
 
     def __init__(self):
         self.zotter_plotter = DrawControl()
@@ -73,7 +80,7 @@ class Parser:
         self.CURRENT_X = x
         self.CURRENT_Y = y
 
-    def parse_and_run(self, filename):
+    def run(self, filename):
         instruction_file = open(filename, 'r')
         Instructions = instruction_file.readlines()
 
@@ -90,8 +97,43 @@ class Parser:
                 y = int(instruction_elem[2])
                 self.execute_reposition(x, y)
 
+    def parse_image(self, path):
+
+        image = cv.imread(path)
+
+        scale_x = self.WIDTH_PAPER / image.shape[1]
+        scale_y = self.HEIGHT_PAPER / image.shape[0]
+
+        imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(imgray, 127, 255, 0)
+
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # cv.drawContours(image, contours, -1, (0, 255, 0), 3)
+        for contour in contours:
+            reposition = False
+            for point in contour[::self.PRECISION]:
+                x = int(round(scale_x * point[0][0].item()))
+                y = int(round(scale_y * point[0][1].item()))
+                if not reposition:
+                    instruction = "reposition " + str(x) + " " + str(y)
+                    self.instructions.append(instruction)
+                    reposition = True
+                else:
+                    instruction = "draw " + str(x) + " " + str(y)
+                    self.instructions.append(instruction)
+
+        file = open("instructions.txt", "w")
+        for instruction in self.instructions:
+            print(instruction)
+            file.write(instruction + "\n")
+        file.close()
+
+        # cv.imshow('Contours', image)
+        # cv.waitKey(0)
+
 
 if __name__ == '__main__':
     parser = Parser()
-    parser.parse_and_run("instructions_peter.txt")
-
+    # parser.run("instructions_peter.txt")
+    # parser.parse_image("images/peter.png")
+    parser.run("instructions.txt")
